996fe39e32ffb560bb835ce5543e717617e9bcc0
Ben White <bwhite2140@outlook.com>
2020-07-15 10:48:36 -0700

    add functionality to make tests work
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,28 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
 
     // properties, methods, etc. will go here.
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
   }
 }
